dfc321b4e65a318d373ae3d52e23dc453dd08021
MMustuf@ <102538395+mustufa6767@users.noreply.github.com>
2022-05-19 14:02:39 -0700

    Add files via upload
    js first assignment

---
+++ app.js
@@ -0,0 +1,13 @@
+
+alert("hello world")
+alert("My name is MUHAMMAD MUSTUFA")
+alert("My father name is MUHAMMAD HANIF")
+alert("I am 18 year old ")
+alert("Certified Mobile Application Development")
+alert("PIZZA")
+alert("sheikh767574@gmail.com")
+alert("A smarter way to learn JavaScript.")
+
+
+
+
